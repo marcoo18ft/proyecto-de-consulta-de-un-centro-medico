f1e78dd799aa7f415accdd35c25105087697a3be
Marcoo18ft <marcolefiman20@gmail.com>
2026-09-14 15:59:36 -0300

    LISTO

--- datos/sql/2026/09/08_14_25_DDL_centro_medico.sql
+++ datos/sql/2026/09/08_14_25_DDL_centro_medico.sql
@@ -1,5 +1,23 @@
-CREATE DATABASE turnos_medico;
+CREATE DATABASE hospital;
 
-USE turnos_medico;
+USE  hospital;
 
+
+
+CREATE TABLE MEDICO
+(
+    rut INT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    ciudad VARCHAR(100),
+    salario INT,
+    especialidad VARCHAR(50),
+    fecharegistro DATE
+
+);
+
+
+INSERT INTO MEDICO (rut, nombre, apellido, email, ciudad, salario, especialidad, fecharegistro)
+VALUES ("22.465.271-3", "pepito", "gonzales"," pepito.gonzales@gmail.com", "temuco", 1500000, "neuro_cirugia", "17-06-1990");
 
