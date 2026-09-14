2290c7013f7ca8733d487fc2bc84cf3068c79bb1
Marcoo18ft <marcolefiman20@gmail.com>
2026-09-14 16:00:45 -0300

    LISTO

--- datos/sql/2026/09/08_14_25_DDL_centro_medico.sql
+++ datos/sql/2026/09/08_14_25_DDL_centro_medico.sql
@@ -21,3 +21,4 @@ CREATE TABLE MEDICO
 INSERT INTO MEDICO (rut, nombre, apellido, email, ciudad, salario, especialidad, fecharegistro)
 VALUES ("22.465.271-3", "pepito", "gonzales"," pepito.gonzales@gmail.com", "temuco", 1500000, "neuro_cirugia", "17-06-1990");
 
+NH
