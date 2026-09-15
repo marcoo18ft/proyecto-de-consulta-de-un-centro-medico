9ed3b7b552373f33aa400a8ca7145f072076c28e
javier <javierinalef24@gmail.com>
2026-09-15 14:39:53 -0300

    xd

---
+++ datos/sql/2026/09/15-09-2026_insert_info.sql
@@ -0,0 +1,2 @@
+INSERT INTO MEDICO (rut, nombre, apellido, email, ciudad, salario, especialidad, fecharegistro)
+VALUES ("22.465.271-3", "pepito", "gonzales"," pepito.gonzales@gmail.com", "temuco", 1500000, "neuro_cirugia", "17-06-1990");
